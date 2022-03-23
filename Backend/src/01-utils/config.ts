d95d6bfcd537fda39a9eaa77f8468102a318b982
eransam <eransam21@gmail.com>
2022-03-23 15:49:45 +0200

    eran

--- Backend/src/01-utils/config.ts
+++ Backend/src/01-utils/config.ts
@@ -9,7 +9,7 @@ class DevelopmentConfig extends Config {
 
 class ProductionConfig extends Config {
     public isDevelopment = false;
-    public mysql = { host: "localhost", user: "root", password: "", database: "MoviesDB" };
+    public mysql = { host: "eu-cdbr-west-02.cleardb.net", user: "B6e77e9e58cdc4", password: "92e35402", database: "heroku_083bd8aabd2cc00" };
 }
 
 const config = process.env.NODE_ENV === "production" ? new ProductionConfig() : new DevelopmentConfig();
--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -8,15 +8,29 @@ import config from "./01-utils/config";
 import errorsHandler from "./02-middleware/errors-handler";
 import ErrorModel from "./03-models/error-model";
 import controller from "./06-controllers/controllers";
+import path from "path";
 
 const server = express();
 
 if (config.isDevelopment) server.use(cors());
 server.use(express.json());
+
+//שורות אלה בעצם מצביעות על הדף הראשי של הפרוייקט שלנו וכך בעצם כאשר הפרוייקט שלנו יהיה באויר דף זה 
+//יפתח כדף הראשי שלנו 
+//הנתיב המלא עד התיקייה הנוחכית __dirname
+const fronendDir = path.join(__dirname, "07-front");
+server.use(express.static(fronendDir));
+
 server.use("/api", controller);
 
 server.use("*", (request: Request, response: Response, next: NextFunction) => {
-    next(new ErrorModel(404, "Route not found."));
+    if(config.isDevelopment){
+        next(new ErrorModel(404, "Route not found."));
+    }
+    else{
+        const indexHtmlFile = path.join(__dirname, "07-front", "index.html");
+        response.sendFile(indexHtmlFile);
+    }
 });
 
 server.use(errorsHandler);
